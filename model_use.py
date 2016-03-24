8672fdd421b4f670e6445badc2eeb5ec6f2569e0
Daniel Garnacho <garnachod@gmail.com>
2016-03-24 10:54:22 +0100

    test y si ha visto la pelicula el rating que ha dado

--- model_use.py
+++ model_use.py
@@ -21,18 +21,37 @@ def readMoviesData(filename):
 
 	return dic
 
+def getFilmsAndRating():
+	users = None
+	with open("train/ratings_parsed.txt", "r") as fin:
+			users = json.loads(fin.read())
 
+	return users
+
+def isFilmInUser_getRating(user, film):
+	for rating in user["ratings"]:
+		if rating[0] == film:
+			return rating[1]
+
+	return False
 if __name__ == '__main__':
 	model = Doc2Vec.load("out.model")
 	dicMovies = readMoviesData("movies.dat")
-	idMovie = "1"
+	idMovie = "13"
 	print "Similar to:" + dicMovies[idMovie].encode("ascii","ignore")
 	#objeto de tipo DocvecsArray
-	for film in model.vocab:
-		print model[film]
+	#for film in model.vocab:
+	#	print model[film]
 
-	for pelicula, coseno in model.most_similar(positive=["1"], topn=20000):
+	for pelicula, coseno in model.most_similar(positive=["13"], topn=20):
 		if math.isnan(coseno) == False:
-			print dicMovies[pelicula].encode("ascii","ignore") + "\t\t" + str(coseno)
+			print pelicula + "\t\t" + dicMovies[pelicula].encode("ascii","ignore") + "\t\t" + str(coseno)
 	
 
+	print "similaridad a la persona 622\n"
+	user = getFilmsAndRating()["622"]
+
+	for pelicula, coseno in model.most_similar(positive=model.docvecs[model.docvecs.doctags["u_622"]], topn=20):
+		if math.isnan(coseno) == False:
+			rating = isFilmInUser_getRating(user, pelicula)
+			print "|" + pelicula + "|" + dicMovies[pelicula].encode("ascii","ignore") + "|" + str(coseno) + "|" + str(rating) + "|"
